be53ab9825dd2484c1e921e39ecf184270e1b6ce
ghosts-arch <ronan.questier@mailfence.com>
2025-04-05 23:13:41 +0200

    async initDatabase method

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,5 @@
-import type { Handle, ServerInit } from "@sveltejs/kit";
+import type { ServerInit } from "@sveltejs/kit";
 import { initDatabase } from "$lib/server/database/database";
 export const init: ServerInit = async () => {
-  initDatabase();
+  await initDatabase();
 };
